54fd24e94c8885a593ddc5ad0e6d70da3eef830d
Espen Scheuer <espenscheuer@gmail.com>
2020-02-26 14:27:11 -0800

    fixed spacing

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,23 +5,15 @@ function Footer() {
 
     return (
         <header className="App-footer">
-          <div className = 'left'>
-          
-                <div className = 'tag'>  
-                    BUILT IN REACT
-                </div>
-           
-            </div>
-            <div className = 'right'>
-                <div className = "footer-link"> 
-                <div className = 'link-wrap'>
-                    <a className = 'link' href="https://github.com/espenscheuer">GITHUB</a>
-                </div>
-                <div className = 'link-wrap'>
-                    <a className = 'link' href="https://www.linkedin.com/in/espen-scheuer/">LINKEDIN</a>
-                </div>
-                </div>
-            </div>
+          BUILT IN REACT
+        <div className = "footer-link">
+        <div className = 'link-wrap'>
+            <a className = 'link' href="https://github.com/espenscheuer">GITHUB</a>
+        </div>
+        <div className = 'link-wrap'>
+            <a className = 'link' href="https://www.linkedin.com/in/espen-scheuer/">LINKEDIN</a>
+        </div>
+        </div>
         </header>
     )
 }
