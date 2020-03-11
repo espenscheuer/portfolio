4cf0fa4765499fc72be185bd0ae382677dc0615a
Espen Scheuer <espenscheuer@gmail.com>
2020-03-11 08:25:57 -0700

    added scroll back

--- src/components/mountains.js
+++ src/components/mountains.js
@@ -4,7 +4,9 @@ import '../App.css';
 function Mountains() {
 
     return (
+        <Parallax className="custom-class" x={[-10, 10]} tagOuter="figure">
         <img className = 'mountains' src={('/img/mountains.svg')} alt ='mountains' />
+        </Parallax>
     )
 }
 
